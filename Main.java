1ad2443046003c0dadd0637a4f39510022d76c4e
ANKIT KUMAR <88582979+BABAR1532@users.noreply.github.com>
2022-08-16 16:32:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Area Of Circle Java Program
+    /*
+     * System.out.println("Enter the radius of circle:");
+     * double radius = sc.nextInt();
+     * double area = 3.14 * radius * radius;
+     * System.out.println("Area of circle is :" + area);
+     */
+    // Area of triangle
+    /*
+     * System.out.println("Enter height and base of triangle: ");
+     * int height = sc.nextInt();
+     * int base = sc.nextInt();
+     * int area = height * base / 2;
+     * System.out.println("Area of triangle is: " + area);
+     */
+
+    // Write a java program to find maximum between three numbers.
+
+    /*
+     * System.out.println("Enter your favorite 3 number:");
+     * int num1 = sc.nextInt();
+     * int num2 = sc.nextInt();
+     * int num3 = sc.nextInt();
+     * int max = num1 ;
+     * if (max < num2 ){
+     * max = num2;
+     * }
+     * if ( max < num3){
+     * max = num3;
+     * }
+     * System.out.println(max);
+     */ // good program
+
+    // Write a java program to check whether a number is negative, positive or zero.
+
+    /*
+     * System.out.println("Enter your number:");
+     * int num = sc.nextInt();
+     * if (num > 0){
+     * System.out.println("Number is positive");
+     * }
+     * else if (num < 0){
+     * System.out.println("Number is negative");
+     * }
+     * else{
+     * System.out.println("its zero");
+     * }
+     */
+
+    // java program to check whether a number is divisible by 5 and 11 or not
+
+    /*
+     * System.out.println("Enter a number");
+     * int num = sc.nextInt();
+     * if (num % 5 == 0 && num %11 == 0){
+     * System.out.println("Number is divisible by 5 and 11");
+     * 
+     * }
+     * else{
+     * System.out.println("Number is not divisible by 5 and 11");
+     * }
+     */
+
+    // Write a java program to check whether a number is even or odd.
+
+    /*
+     * System.out.println("Enter your no:");
+     * int num = sc.nextInt();
+     * 
+     * if (num%2==0){
+     * System.out.println("No is even");}
+     * 
+     * else{
+     * System.out.println("No is odd");
+     * }
+     */
+
+    // Write a java program to check whether a year is leap year or not.
+
+    /*
+     * System.out.println("Enter the year");
+     * int year = sc.nextInt();
+     * if(year % 4 == 0 || year % 400==0){
+     * System.out.println("The year is leap year");
+     * }
+     * else{
+     * System.out.println("The year is not a leap year");
+     * }
+     */
+
+  }
+}
